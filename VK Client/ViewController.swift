6b7568bf4e27bf242e71f45cc4d30adb63226b84
Vlad Yandola <vladan2000.9@mail.ru>
2020-06-21 03:39:54 +0500

    Initial Commit

---
+++ VK Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Client
+//
+//  Created by Const. on 21.06.2020.
+//  Copyright © 2020 Oleginc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
